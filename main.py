2155c28fe7a4e213c516efdac4750dcb81d33bc5
Yunseong Yang <abee3417@naver.com>
2024-05-11 14:58:07 +0900

    Activate line 165 to 167

--- main.py
+++ main.py
@@ -162,6 +162,6 @@ async def crawl_data(field: str = Query(None), person: str = Query(None), sort: 
         raise HTTPException(status_code=500, detail="An unexpected error occurred")
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
