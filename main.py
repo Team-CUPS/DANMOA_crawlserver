007d4603548549a99e133c58642cfe944b126451
Yunseong Yang <abee3417@naver.com>
2024-05-16 13:39:50 +0900

    Fix datetime error

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from fastapi import FastAPI, HTTPException, Query
 
 # 날짜 설정
 ymd = pytz.timezone('Asia/Seoul')
-day_code = datetime.now(ymd).weekday() # 요일 코드 생성 (월~일 : 0~6)
-ymd = str(datetime.now(ymd))
+ymd = datetime.datetime.now(ymd)
+day_code = ymd.weekday() # 요일 코드 생성 (월~일 : 0~6)
+ymd = str(ymd)
 ymd = datetime(int(ymd[:4]), int(ymd[5:7]), int(ymd[8:10])) # 오늘 날짜 데이터 생성 (yyyy.mm.dd)
 
 # fastAPI 실행
