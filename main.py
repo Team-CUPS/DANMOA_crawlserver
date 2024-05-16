56e32b1da1e952f60e397f49452bbc2f517f5740
Yunseong Yang <abee3417@naver.com>
2024-05-16 13:54:15 +0900

    Fix datetime error

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from typing import Dict
 import re
 import httpx
-import datetime
+from datetime import datetime
 import pytz
 from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException, Query
 
 # 날짜 설정
 ymd = pytz.timezone('Asia/Seoul')
-ymd = datetime.datetime.now(ymd)
+ymd = datetime.now(ymd)
 day_code = ymd.weekday() # 요일 코드 생성 (월~일 : 0~6)
 ymd = str(ymd)
 ymd = datetime(int(ymd[:4]), int(ymd[5:7]), int(ymd[8:10])) # 오늘 날짜 데이터 생성 (yyyy.mm.dd)
